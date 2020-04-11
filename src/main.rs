3979280d55d58c8c789b0fac7f7b64b0455b3a88
GaruGaru <garuglieritommaso.dev@gmail.com>
2020-04-11 22:06:58 +0200

    mock api

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+extern crate rocket_contrib;
+
+#[macro_use]
+extern crate rocket;
+
+use rocket_contrib::json::{Json};
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct Repository {
+    name: String,
+    description: String,
+}
+
+#[get("/")]
+fn index() -> Json<Vec<Repository>> {
+    Json(vec![
+        Repository {
+            name: "test".to_string(),
+            description: "this is a really nice project".to_string(),
+        },
+        Repository
+        {
+            name: "test".to_string(),
+            description: "this is a really nice project".to_string(),
+        }
+    ])
+}
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite()
+        .mount("/", routes![index])
+        .launch();
 }
